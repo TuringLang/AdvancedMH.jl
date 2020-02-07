5189c3841f524c7647fdf2713fb49fa8c6eb0064
Cameron Pfiffer <cpfiffer@gmail.com>
2020-02-06 18:20:01 -0800

    Add docstring

--- src/mh-core.jl
+++ src/mh-core.jl
@@ -29,6 +29,17 @@ The sampler is constructed using
 ```julia
 spl = MetropolisHastings(proposal)
 ```
+
+When using `MetropolisHastings` with the function `sample`, the following keyword
+arguments are allowed:
+
+- `init_params` defines the initial parameterization for your model. If
+none is given, the initial parameters will be drawn from the sampler's proposals.
+- `param_names` is a vector of strings to be assigned to parameters. This is only
+used if `chain_type=Chains`.
+- `chain_type` is the type of chain you would like returned to you. Supported
+types are `chain_type=Chains` if `MCMCChains` is imported, or 
+`chain_type=StructArray` if `StructArrays` is imported.
 """
 mutable struct MetropolisHastings{D} <: Metropolis
     proposal :: D
