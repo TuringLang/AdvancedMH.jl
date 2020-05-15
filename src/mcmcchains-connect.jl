75e66aedafd376bf5c6c7707661e8396e3ea9c98
Cameron Pfiffer <cpfiffer@gmail.com>
2020-05-15 06:42:18 -0700

    Remove some commented code

--- src/mcmcchains-connect.jl
+++ src/mcmcchains-connect.jl
@@ -29,43 +29,6 @@ function AbstractMCMC.bundle_samples(
     return Chains(vals, param_names, (internals=["lp"],))
 end
 
-# function AbstractMCMC.bundle_samples(
-#     rng::Random.AbstractRNG, 
-#     model::DensityModel, 
-#     s::Ensemble, 
-#     N::Integer, 
-#     ts::Vector{<:Walker},
-#     chain_type::Type{Chains}; 
-#     param_names=missing,
-#     kwargs...
-# )
-#     # return ts
-#     vals = mapreduce(
-#         t -> map(i -> vcat(ts[t].walkers[i].params, 
-#                  ts[t].walkers[i].lp, t, i),
-#                  1:length(ts[t].walkers)), 
-#         vcat, 
-#         1:length(ts))
-    
-#     vals = Array(reduce(hcat, vals)')
-
-#     # return vals
-
-#     # Check if we received any parameter names.
-#     if ismissing(param_names)
-#         param_names = ["param_$i" for i in 1:length(ts[1].walkers[1].params)]
-#     else
-#         # Deepcopy to be thread safe.
-#         param_names = deepcopy(param_names)
-#     end
-
-#     # Add the log density field to the parameter names.
-#     push!(param_names, "lp", "iteration", "walker")
-
-#     # Bundle everything up and return a Chains struct.
-#     return Chains(vals, param_names, (internals=["lp", "iteration", "walker"],))
-# end
-
 function AbstractMCMC.bundle_samples(
     rng::Random.AbstractRNG, 
     model::DensityModel, 
