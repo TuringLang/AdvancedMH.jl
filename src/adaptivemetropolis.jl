46f77ca789c81d63b93b924a04d34866eedce55f
kaandocal <>
2021-05-26 23:14:00 +0100

    Added Welford's algorithm for adaptive covariance calculations

--- src/adaptivemetropolis.jl
+++ src/adaptivemetropolis.jl
@@ -11,8 +11,9 @@ mutable struct AMProposal{FT <: Real, CT <: AbstractMatrix{FT},
     epsilon::CT
     scalefactor::FT
     proposal::MNT
-    samplemean::Vector{FT}
-    samplesqmean::Matrix{FT}
+    μ::Vector{FT}
+    M::Matrix{FT}
+    δ::Vector{FT}
     N::Int
 end
 
@@ -20,30 +21,30 @@ function AMProposal(epsilon::AbstractMatrix{FT},
                     scalefactor=FT(2.38^2 / size(epsilon, 1))) where {FT <: Real}
     sym = PDMat(Symmetric(epsilon))
     proposal = MvNormal(zeros(size(sym, 1)), sym)
-    AMProposal(sym, scalefactor, proposal, mean(proposal), zeros(size(sym)...), 0)
+    AMProposal(sym, scalefactor, proposal, zeros(size(sym,1)), zeros(size(sym)...), 
+               zeros(size(sym,1)), 0)
 end
 
 logratio_proposal_density(::AMProposal, params_prev, params) = 0
 
 # When the proposal is initialised the empirical posterior covariance is zero
 function trackstep(proposal::AMProposal, params)
-    proposal.samplemean .= params
-    proposal.samplesqmean .= params * params'
-    proposal.proposal = MvNormal(zeros(size(proposal.samplemean)), proposal.epsilon)
+    proposal.μ .= params
+    proposal.M .= 0
+    proposal.proposal = MvNormal(zeros(size(proposal.μ)), proposal.epsilon)
     proposal.N = 1
 end
 
 # Recompute the empirical posterior covariance matrix
 function trackstep(proposal::AMProposal, params, ::Union{Val{true}, Val{false}})
-    proposal.samplemean .= (proposal.samplemean .* proposal.N .+ params) ./ (proposal.N + 1)
-
-    proposal.samplesqmean .= (proposal.samplesqmean .* proposal.N + params * params') ./ 
-                             (proposal.N + 1)
-
-    samplecov = proposal.samplesqmean .- proposal.samplemean * proposal.samplemean'
-    prop_cov = proposal.scalefactor .* samplecov
-    proposal.proposal = MvNormal(mean(proposal.proposal), prop_cov .+ proposal.epsilon)
     proposal.N += 1
+    proposal.δ .= params .- proposal.μ
+    proposal.μ .+= proposal.δ ./ proposal.N
+
+    mul!(proposal.M, params .- proposal.μ, proposal.δ', 1.0, 1.0)
+
+    prop_cov = proposal.M .* proposal.scalefactor ./ proposal.N .+ proposal.epsilon
+    proposal.proposal = MvNormal(mean(proposal.proposal), Symmetric(prop_cov))
 end
 
 function propose(rng::Random.AbstractRNG, p::AMProposal, ::DensityModel)
@@ -53,4 +54,3 @@ end
 function propose(rng::Random.AbstractRNG, p::AMProposal, ::DensityModel, t)
     return t + rand(rng, p.proposal)
 end
-
