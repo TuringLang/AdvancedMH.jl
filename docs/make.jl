511978c864c72b88cb6a3b20af48ad7f77310e97
jaimerz <jaimerz011235813@gmail.com>
2023-07-13 17:13:57 +0200

    documenter

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+using Pkg
+
+using Documenter
+using AdvancedMH
+
+# cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "src/index.md"))
+
+makedocs(sitename = "AdvancedMH", format = Documenter.HTML(), modules = [AdvancedMH])
+
+deploydocs(
+    repo = "github.com/TuringLang/AdvancedMH.jl.git",
+    push_preview = true, # allow PR to deploy
+)
---
+++ docs/src/api.md
@@ -0,0 +1,22 @@
+# AdvancedMH.jl
+
+Documentation for AdvancedMH.jl
+
+```@contents
+```
+
+## Structs
+```@docs
+StaticMH
+```
+
+## Functions
+
+```@docs
+sample
+```
+
+## Index
+
+```@index
+```
