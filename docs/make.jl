3a386038026f07f0c06a23f41d73114dd4fbaa7b
Shravan Goswami <123811742+shravanngoswamii@users.noreply.github.com>
2025-02-09 16:07:43 +0530

    Documentation and Turing Navigation CI improvement (#110)
    * Update and rename documentation.yml to Docs.yml
    
    * Create DocsNav.yml
    
    * No need of deploydocs() after using new Docs & DocsNav workflows
    
    * branch name fix

--- docs/make.jl
+++ docs/make.jl
@@ -6,8 +6,3 @@ using AdvancedMH
 # cp(joinpath(@__DIR__, "../README.md"), joinpath(@__DIR__, "src/index.md"))
 
 makedocs(sitename = "AdvancedMH", format = Documenter.HTML(), modules = [AdvancedMH], checkdocs = :exports)
-
-deploydocs(
-    repo = "github.com/TuringLang/AdvancedMH.jl.git",
-    push_preview = true, # allow PR to deploy
-)
